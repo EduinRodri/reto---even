a2ae12669bb60b005bb8295073a7e50a2d30122b
EduinRodri <eduin.rodri.7@gmail.com>
2025-02-21 23:11:15 -0500

    getter & setter

--- model/carrito.py
+++ model/carrito.py
@@ -1,6 +1,17 @@
 class Carrito:
     def __init__(self, id_usuario, id_producto, cantidad):
-        self.id_usuario = id_usuario
-        self.id_producto = id_producto
-        self.cantidad = cantidad
-        
+        self.__id_usuario = id_usuario
+        self.__id_producto = id_producto
+        self.__cantidad = cantidad
+
+    def get_id_usuario(self):
+        return self.__id_usuario
+
+    def get_id_producto(self):
+        return self.__id_producto
+
+    def get_cantidad(self):
+        return self.__cantidad
+    
+    def set_cantidad(self, cantidad):
+        self.__cantidad = cantidad
--- model/producto.py
+++ model/producto.py
@@ -1,7 +1,26 @@
 class Producto:
     def __init__(self, nombre, precio, stock):
-        self.nombre = nombre
-        self.precio = precio
-        self.stock = stock
+        self.__nombre = nombre
+        self.__precio = precio
+        self.__stock = stock
+
+    # Getters y Setters
+    def get_nombre(self):
+        return self.__nombre
+    
+    def get_precio(self):
+        return self.__precio
+    
+    def get_stock(self):
+        return self.__stock
+    
+    def set_stock(self, stock):
+        self.__stock = stock
+        
+    def set_precio(self, precio):
+        self.__precio = precio  
+        
+    def set_nombre(self, nombre):
+        self.__nombre = nombre
 
     
